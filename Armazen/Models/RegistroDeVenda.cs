37a7663404f420c9bf5a55708df44a60bb3c5187
João Vitor de Oliveira Ferreira <olibeira88@hotmail.com>
2022-08-24 16:50:25 -0300

    Adição de novos atributos

---
+++ Armazen/Models/RegistroDeVenda.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Armazen.Models
+{
+    public class RegistroDeVenda
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public double Quantia { get; set; }
+        public EstadoDeVenda Status { get;set }
+    }
+}
---
+++ Armazen/Models/Vendedor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Armazen.Models
+{
+    public class Vendedor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime Aniversario { get; set; }
+        public double SalarioBase { get; set; }
+
+
+    }
+}
